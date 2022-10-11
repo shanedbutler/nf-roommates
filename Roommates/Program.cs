fa35fee9d34cbd274f4f62e52861f1824a9bb0dc
shanedbutler <shanedbutler@gmail.com>
2022-10-11 15:34:12 -0400

    Initial code

---
+++ Roommates/Program.cs
@@ -0,0 +1,52 @@
+﻿using Roommates.Repositories;
+using Roommates.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Roommates
+{
+    internal class Program
+    {
+        /// <summary>
+        ///  This is the address of the database.
+        ///  We define it here as a constant since it will never change.
+        /// </summary>
+        private const string CONNECTION_STRING = @"server=localhost\SQLExpress;database=Roommates;integrated security=true;TrustServerCertificate=True";
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            RoomRepository roomRepo = new RoomRepository(CONNECTION_STRING);
+
+            Room singleRoom = roomRepo.GetById(1);
+            Console.WriteLine($"Single room, {singleRoom.Name} has an Id of {singleRoom.Id} and a max occupancy of {singleRoom.MaxOccupancy}");
+
+            Room bathroom = new Room
+            {
+                Name = "Bathroom",
+                MaxOccupancy = 1
+            };
+
+            roomRepo.Insert(bathroom);
+
+            bathroom.MaxOccupancy = 3;
+            bathroom.Name = "Primary Bathroom";
+            roomRepo.Update(bathroom);
+
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine($"Updated the Room with id {bathroom.Id}");
+
+            roomRepo.Delete(bathroom.Id);
+
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine($"Deleted the Room with id {bathroom.Id}");
+
+
+            List<Room> allRooms = roomRepo.GetAll();
+            Console.WriteLine("\nGetting All Rooms:");
+            allRooms.ForEach(room => Console.WriteLine($"{room.Name} has an Id of {room.Id} and a max occupancy of {room.MaxOccupancy}"));
+
+        }
+    }
+}
---
+++ Roommates/Repositories/RoommateRepository.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Data.SqlClient;
+using Roommates.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roommates.Repositories
+{
+    internal class RoommateRepository
+    {
+
+    }
+}
